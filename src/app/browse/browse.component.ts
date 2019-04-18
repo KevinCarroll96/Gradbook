39321e95b62b8303b7cde0ec466850f179eff9e9
HellPwnage1337 <hellpwnage1337@gmail.com>
2019-04-18 12:35:15 -0500

    Worked on Favorites screen

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+//import { Kinvey, User } from 'kinvey-nativescript-sdk';
 
 @Component({
-    selector: "Browse",
+    selector: "Search",
     moduleId: module.id,
-    templateUrl: "./browse.component.html"
+    templateUrl: "./search.component.html"
 })
-export class BrowseComponent implements OnInit {
+export class SearchComponent implements OnInit {
 
     constructor() {
         // Use the component constructor to inject providers.
@@ -21,4 +22,58 @@ export class BrowseComponent implements OnInit {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+    /*
+	public food()
+	{
+		const query = new Kinvey.Query();
+		query.equalTo('category', 'Food');
+		const subscription = dataStore.find(query)
+		  .subscribe((entities: {}[]) => {
+			// ...
+		  }, (error: Kinvey.BaseError) => {
+			// ...
+		  }, () => {
+			// ...
+		  });
+	}
+	public ent()
+	{
+		const query = new Kinvey.Query();
+		query.equalTo('category', 'Entertainment');
+		const subscription = dataStore.find(query)
+		  .subscribe((entities: {}[]) => {
+			// ...
+		  }, (error: Kinvey.BaseError) => {
+			// ...
+		  }, () => {
+			// ...
+		  });
+	}
+	public campus()
+	{
+		const query = new Kinvey.Query();
+		query.equalTo('category', 'Campus');
+		const subscription = dataStore.find(query)
+		  .subscribe((entities: {}[]) => {
+			// ...
+		  }, (error: Kinvey.BaseError) => {
+			// ...
+		  }, () => {
+			// ...
+		  });
+	}
+	public lodging()
+	{
+		const query = new Kinvey.Query();
+		query.equalTo('category', 'Lodging');
+		const subscription = dataStore.find(query)
+		  .subscribe((entities: {}[]) => {
+			// ...
+		  }, (error: Kinvey.BaseError) => {
+			// ...
+		  }, () => {
+			// ...
+		  });
+	}
+	*/
 }
